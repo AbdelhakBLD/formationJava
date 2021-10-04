b5c5f16d35e3a9d008478054b63cced457cfd049
AbdelhakBLD <abdelhak.belaid@outlook.com>
2021-10-04 14:12:26 +0200

    exo 3 et 4

--- exercice/src/fr/dessin/exercice/Main.java
+++ exercice/src/fr/dessin/exercice/Main.java
@@ -3,9 +3,19 @@ package fr.dessin.exercice;
 public class Main {
 
     public static void main(String[] args) {
-        Point2D point = new Point2D();
-        point.afficher();
-        point.translate(2, 2);
-        point.afficher();
+        Point2D point1 = new Point2D();
+        point1.afficher();
+        point1.setX(4);
+        point1.setY(6);
+        point1.afficher();
+        point1.translate(2, 2);
+        point1.afficher();
+
+        Point2D point2 = new Point2D(1,2);
+        point2.afficher();
+        point2.translate(2, 2);
+        point2.afficher();
+
+        Point2D.getCompteur();
     }
 }
--- exercice/src/fr/dessin/exercice/Point2D.java
+++ exercice/src/fr/dessin/exercice/Point2D.java
@@ -2,8 +2,21 @@ package fr.dessin.exercice;
 
 public class Point2D {
 
-    private Integer x = 5;
-    private Integer Y = 10;
+    private static Integer compteur = 0;
+    private Integer x = 0;
+    private Integer Y = 0;
+
+    public Point2D() {
+        setX(0);
+        setY(0);
+        Point2D.compteur += 1;
+    }
+
+    public Point2D(Integer vX, Integer vY) {
+        setX(vX);
+        setY(vY);
+        Point2D.compteur += 1;
+    }
 
     public Integer getX() {
         return x;
@@ -21,12 +34,25 @@ public class Point2D {
         Y = y;
     }
 
+    /**
+     *  permet d'afficher un point
+     */
     public void afficher() {
         System.out.println('[' + getX().toString() + ", " + getY().toString() + ']');
     }
 
+    /**
+     * Methode publique qui permet de translater X et Y
+     *
+     * @param dX Integer
+     * @param dY Integer
+     */
     public void translate(Integer dX, Integer dY) {
         setX(getX() + dX);
         setY(getY() + dY);
+    }
+
+    public static Integer getCompteur() {
+        return Point2D.compteur;
     }
 }
